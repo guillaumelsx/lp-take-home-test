087764e1425d053cde0d972d3018d7edb5df7d7b
Guillaume Loiseleux <guillaumeloiseleux@MacBook-Pro-de-Guillaume.local>
2024-07-17 16:41:09 +0200

    minor refacto when building the log to use structuredClone

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const pharmacy = new Pharmacy(drugs);
 const log = [];
 
 for (let elapsedDays = 0; elapsedDays < 30; elapsedDays++) {
-  log.push(JSON.parse(JSON.stringify(pharmacy.updateBenefitValue())));
+  const updatedBenefits = structuredClone(pharmacy.updateBenefitValue());
+  log.push(updatedBenefits);
 }
 
 /* eslint-disable no-console */
