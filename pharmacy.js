851c5515459b015004b8e45152204a3da20855c0
Guillaume Loiseleux <guillaumeloiseleux@MacBook-Pro-de-Guillaume.local>
2024-07-17 17:26:57 +0200

    created separate update functions for each drug

--- pharmacy.js
+++ pharmacy.js
@@ -19,38 +19,47 @@ export class Pharmacy {
       if (drug.name === "Magic Pill") continue;
 
       drug.expiresIn--;
-
-      switch (drug.name) {
-        case "Herbal Tea":
-          if (drug.expiresIn < 0) {
-            drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 2);
-          } else {
-            drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 1);
-          }
-          break;
-
-        case "Fervex":
-          if (drug.expiresIn < 0) {
-            drug.benefit = 0;
-          } else if (drug.expiresIn < 5) {
-            drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 3);
-          } else if (drug.expiresIn < 10) {
-            drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 2);
-          } else {
-            drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 1);
-          }
-          break;
-
-        default:
-          if (drug.expiresIn < 0) {
-            drug.benefit = Math.max(MIN_BENEFIT, drug.benefit - 2);
-          } else {
-            drug.benefit = Math.max(MIN_BENEFIT, drug.benefit - 1);
-          }
-          break;
-      }
+      this.updateDrug(drug);
     }
 
     return this.drugs;
+  }
+
+  updateDrug(drug) {
+    const updateStrategies = {
+      "Herbal Tea": this.updateHerbalTea,
+      Fervex: this.updateFervex,
+      default: this.updateDefaultDrug,
+    };
+
+    const updateStrategy =
+      updateStrategies[drug.name] || updateStrategies["default"];
+    updateStrategy.call(this, drug);
+  }
+
+  updateHerbalTea(drug) {
+    drug.benefit =
+      drug.expiresIn < 0
+        ? Math.min(MAX_BENEFIT, drug.benefit + 2)
+        : Math.min(MAX_BENEFIT, drug.benefit + 1);
+  }
+
+  updateFervex(drug) {
+    if (drug.expiresIn < 0) {
+      drug.benefit = 0;
+    } else if (drug.expiresIn < 5) {
+      drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 3);
+    } else if (drug.expiresIn < 10) {
+      drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 2);
+    } else {
+      drug.benefit = Math.min(MAX_BENEFIT, drug.benefit + 1);
+    }
+  }
+
+  updateDefaultDrug(drug) {
+    drug.benefit =
+      drug.expiresIn < 0
+        ? Math.max(MIN_BENEFIT, drug.benefit - 2)
+        : Math.max(MIN_BENEFIT, drug.benefit - 1);
   }
 }
